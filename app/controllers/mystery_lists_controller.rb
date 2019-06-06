8c3995adb474bfe04b41c8bfd918b826a5c3bdd0
kitahara6 <kitahara@sackle.co.jp>
2019-06-06 22:48:11 +0900

    コメント追記・修正

--- app/controllers/mystery_lists_controller.rb
+++ app/controllers/mystery_lists_controller.rb
@@ -1,5 +1,6 @@
 class MysteryListsController < ApplicationController
 
+  # 謎解き問題一覧(難易度別)
   def index
     @basics = Post.all.where(challenge_level: ::Post.challenge_levels[:basic])
     @mediums = Post.all.where(challenge_level: ::Post.challenge_levels[:medium])
