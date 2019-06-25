fd24db60a19e519b5cf64a0428cea0c548ba5346
kitahara6 <kitahara@sackle.co.jp>
2019-06-25 18:50:02 +0900

    ユーザリスト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  ##
+  # リレーション
+  ##
+  has_many :posts
 
+  ##
+  # バリデーション
+  ##
   validates :username, presence: true, length: { maximum: 50 }
+
+  ##
+  # scopes
+  ##
+  # 投稿(内部)
+  scope :with_inner_posts, -> { joins(:posts).eager_load(:posts) }
+
+  # 投稿(外部)
+  scope :with_posts, -> { eager_load(:posts) }
 end
